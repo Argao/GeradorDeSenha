036d476d6490a83f8f20771e3527565ea0b9a165
Aragao <jgabrielaragao@gmail.com>
2024-06-27 22:03:03 -0300

    Tela principal

--- app/src/main/java/com/example/geradordesenha/MainActivity.java
+++ app/src/main/java/com/example/geradordesenha/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.geradordesenha;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nome;
+    EditText dataString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nome = (EditText) findViewById(R.id.input_nome);
+        dataString = (EditText) findViewById(R.id.input_data);
+    }
+
+
+
+    public void trocarTela(View view) {
+
     }
 }
